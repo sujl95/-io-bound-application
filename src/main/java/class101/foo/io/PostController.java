a8548c1fce81b038f18396fd727892113210de69
sujl95 <sujl95@naver.com>
2021-04-17 12:48:56 +0900

    [#1] pageing 적용

--- src/main/java/class101/foo/io/PostController.java
+++ src/main/java/class101/foo/io/PostController.java
@@ -1,15 +1,19 @@
 package class101.foo.io;
 
-import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.List;
+import lombok.RequiredArgsConstructor;
 
 @RestController
+@RequiredArgsConstructor
 public class PostController {
 
-    @Autowired
-    PostRepository postRepository;
+    private final static Integer PAGE_SIZE = 20;
+
+    private final PostRepository postRepository;
 
     // 1. 글을 작성한다.
     @PostMapping("/post")
@@ -17,10 +21,12 @@ public class PostController {
         return postRepository.save(post);
     }
 
-    // 2-1. 글 목록을 조회한다.
+    // 2. 글 목록을 페이징하여 변환
     @GetMapping("/posts")
-    public List<Post> getPostList() {
-        return postRepository.findAll();
+    public Page<Post> getPostList(@RequestParam(defaultValue = "1") Integer page) {
+        return postRepository.findAll(
+                PageRequest.of(page -1, PAGE_SIZE, Sort.by("id").descending())
+        );
     }
     
     // 2-2 글 목록을 페이징하여 반환
